7b26b8435a87e4d69423c7a81eb6a1918cb63d4e
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2025-04-15 13:28:26 +0200

    Update Added_information.sh

--- Added_information.sh
+++ Added_information.sh
@@ -2,7 +2,21 @@
 DATE="$(date +%a.%d.%b.%Y)"
 echo ">>>H-Ahmed_panelv5 $DATE<<<"
 echo " "
-echo "> 06-04-2025"
+echo "> 15-04-2025"
+sleep 2s
+
+
+echo " "
+echo "> 15-04-2025"
+sleep 2s
+echo "> subssupport 1.7.0-R19 15-04-2025 updated"
+echo "> Ajpanel v10.8.0 15-04-2025 updated"
+echo "> Ajpanel v10.8.1 15-04-2025 updated"
+echo "> xklass plugin v1.47 15-04-2025 updated"
+echo "> xstreamity plugin v4.95 15-04-2025 updated"
+echo "> bouquetmakerxtream 1.48 15-04-2025 updated"
+echo "> Aglare FHD pli _v5.4 skin 15-04-2025 updated"
+echo "> Aglare FHD v5.6 skin 15-04-2025 updated"
 sleep 2s
 
 echo " "
@@ -17,5 +31,5 @@ echo "> bouquetmakerxtream 1.47 06-04-2025 updated"
 echo "> fullhdglass17 skin 9.32 06-04-2025 updated"
 sleep 2s
 
-echo ">>> Last Updatting 06-04-2025 <<<"
+echo ">>> Last Updatting 15-04-2025 <<<"
 exit 0
