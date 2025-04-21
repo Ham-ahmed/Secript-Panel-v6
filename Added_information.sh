cab7f40193884295308cd678588e3f9ebe5d52a5
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2025-04-21 18:38:19 +0200

    Update Added_information.sh

--- Added_information.sh
+++ Added_information.sh
@@ -2,7 +2,18 @@
 DATE="$(date +%a.%d.%b.%Y)"
 echo ">>>H-Ahmed_panelv5 $DATE<<<"
 echo " "
-echo "> 18-04-2025"
+echo "> 21-04-2025"
+sleep 2s
+
+echo " "
+echo "> 21-04-2025"
+sleep 2s
+echo "> 67-xklass Mange playlists 21-04-2025 updated"
+echo "> 67-xstreamity Mange playlists 21-04-2025 updated"
+echo "> Ncam v15.3 r0 New 21-04-2025 updated"
+echo "> bouquetmakerxtream 1.51 21-04-2025 updated"
+echo "> e2iplayer zadmario plugin 21-04-2025 updated"
+echo "> osacamstatus v5.6 21-04-2025 updated"
 sleep 2s
 
 echo " "
@@ -40,5 +51,5 @@ echo "> bouquetmakerxtream 1.47 06-04-2025 updated"
 echo "> fullhdglass17 skin 9.32 06-04-2025 updated"
 sleep 2s
 
-echo ">>> Last Updatting 18-04-2025 <<<"
+echo ">>> Last Updatting 21-04-2025 <<<"
 exit 0
