dbaaeca32e10ebf0be954832f79b97c7dd333444
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2025-08-11 16:20:28 +0300

    Update Added_information.sh

--- Added_information.sh
+++ Added_information.sh
@@ -2,7 +2,18 @@
 DATE="$(date +%a.%d.%b.%Y)"
 echo ">>>H-Ahmed_panelv5 $DATE<<<"
 echo " "
-echo "> 10-08-2025"
+echo "> 11-08-2025"
+sleep 2s
+
+echo " "
+echo "> 11-08-2025"
+sleep 2s
+echo "> servicescanupdates plugin v1.6 11-08-2025 updated"
+echo "> FeedCatcher plugin v1.1 11-08-2025 updated"
+echo "> historyzap plugin v1.0.45 11-08-2025 updated"
+echo "> bouquetmakerxtream 1.60 11-08-2025 updated"
+echo "> aglare-fhd-pli_6.2 Skin 11-08-2025 updated"
+echo "> xDreamy v5.9.9-r95 Skin 11-08-2025 updated"
 sleep 2s
 
 echo " "
@@ -159,68 +170,5 @@ echo "> bouquetmakerxtream 1.52 06-05-2025 updated"
 echo "> fullhdglass17 skin 9.50 06-05-2025 updated"
 sleep 2s
 
-echo " "
-echo "> 29-04-2025"
-sleep 2s
-echo "> auto dcw key add v1.0 29-04-2025 updated"
-echo "> keyadder plugin v8.4 29-04-2025 updated"
-echo "> oscam emu levi45 11882 802 29-04-2025 updated"
-echo "> oscam emu 11881 802 audi06_19 29-04-2025 updated"
-echo "> xklass plugin v1.49 29-04-2025 updated"
-echo "> xstreamity plugin v4.97 29-04-2025 updated"
-sleep 2s
-
-echo " "
-echo "> 21-04-2025"
-sleep 2s
-echo "> PrioFHD sKIN OpenBH 5.5.1.009 21-04-2025 updated"
-sleep 2s
-
-echo " "
-echo "> 21-04-2025"
-sleep 2s
-echo "> 67-xklass Mange playlists 21-04-2025 updated"
-echo "> 67-xstreamity Mange playlists 21-04-2025 updated"
-echo "> Ncam v15.3 r0 New 21-04-2025 updated"
-echo "> bouquetmakerxtream 1.51 21-04-2025 updated"
-echo "> e2iplayer zadmario plugin 21-04-2025 updated"
-echo "> osacamstatus v5.6 21-04-2025 updated"
-sleep 2s
-
-echo " "
-echo "> 18-04-2025"
-sleep 2s
-echo "> youVixFHD v3 Skin Mod H-Ahmed 18-04-2025 updated"
-echo "> xklass plugin v1.48 18-04-2025 updated"
-echo "> xstreamity plugin v4.96 18-04-2025 updated"
-echo "> bouquetmakerxtream 1.48 18-04-2025 updated"
-echo "> levi45multicammanager 10.2-r1 18-04-2025 updated"
-sleep 2s
-
-echo " "
-echo "> 15-04-2025"
-sleep 2s
-echo "> subssupport 1.7.0-R19 15-04-2025 updated"
-echo "> Ajpanel v10.8.0 15-04-2025 updated"
-echo "> Ajpanel v10.8.1 15-04-2025 updated"
-echo "> xklass plugin v1.47 15-04-2025 updated"
-echo "> xstreamity plugin v4.95 15-04-2025 updated"
-echo "> bouquetmakerxtream 1.48 15-04-2025 updated"
-echo "> Aglare FHD pli _v5.4 skin 15-04-2025 updated"
-echo "> Aglare FHD v5.6 skin 15-04-2025 updated"
-sleep 2s
-
-echo " "
-echo "> 06-04-2025"
-sleep 2s
-echo "> subssupport 1.7.0-R18 06-04-2025 updated"
-echo "> xtraevent v6.820 06-04-2025 updated"
-echo "> oscam emu levi45 11878 802 06-04-2025 updated"
-echo "> xklass plugin v1.44 06-04-2025 updated"
-echo "> xstreamity plugin v4.92 06-04-2025 updated"
-echo "> bouquetmakerxtream 1.47 06-04-2025 updated"
-echo "> fullhdglass17 skin 9.32 06-04-2025 updated"
-sleep 2s
-
-echo ">>> Last Updatting 10-08-2025 <<<"
+echo ">>> Last Updatting 11-08-2025 <<<"
 exit 0
