bc6b910031dcd2f5c3d02716c623b8179303c98f
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2025-09-10 20:57:51 +0300

    Add files via upload

--- 1New_update.sh
+++ 1New_update.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
+
 #config
 espp=$(cat /etc/enigma2/settings | grep config.plugins.AJPanel.backupPath | cut -d '=' -f 2)
-
-
 pack="ajpanel_menu.xml"
 package=$espp$pack
 url=https://raw.githubusercontent.com/Ham-ahmed/Secript-Panel-v6/refs/heads/main/ajpanel_menu.xml
-if [ "$espp" == "/media/hdd/AJPanel_Backup/" ]; then
-pack="ajpanel_menu_HA-v6.xml"
-package=$espp$pack
-fi
 
 #download & install
-echo "> installing latest update please wait ..."
-sleep 3s
-wget -O $package --no-check-certificate $url
+wget -qO $package --no-check-certificate $url
+sleep 3
 
+wget "http://localhost/web/message?text=> $(date +%a.%d.%b.%Y),  ajpanel_menu_HA-v6 is updated successfully &type=5&timeout=5" >/dev/null 2>&1
+sleep 5
 
-echo "> done"
-sleep 3s
-exit 0
