fcc45192e2e8b9c1c7df0424440e2c30a9381852
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2025-09-10 22:49:05 +0300

    Update 1New_update.sh

--- 1New_update.sh
+++ 1New_update.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os
-import sys
-import time
-import requests
-import xml.etree.ElementTree as ET
-from pathlib import Path
-
 def main():
     # Config
     try:
